fe4a25d35b91783dd80be57b6361a854dd479aca
Jeremy <ggajy123@gmail.com>
2026-01-04 20:22:07 -0800

    feat: dynamic lighting and pbr

--- Assets/Scripts/VoxelRaytracerFeature.cs
+++ Assets/Scripts/VoxelRaytracerFeature.cs
@@ -57,6 +57,7 @@ public class VoxelRaytracerFeature : ScriptableRendererFeature
     protected override void Dispose(bool disposing)
     {
         CoreUtils.Destroy(_compositeMaterial);
+        _pass?.Dispose();
     }
 
     // --- The Render Pass ---
@@ -71,8 +72,16 @@ public class VoxelRaytracerFeature : ScriptableRendererFeature
         private static readonly int _CameraToWorldParams = Shader.PropertyToID("_CameraToWorld");
         private static readonly int _CameraInverseProjectionParams = Shader.PropertyToID("_CameraInverseProjection");
         private static readonly int _CameraDepthTextureParams = Shader.PropertyToID("_CameraDepthTexture");
-        private static readonly int _GridSizeParams = Shader.PropertyToID("_GridSize"); // New
+        private static readonly int _GridSizeParams = Shader.PropertyToID("_GridSize"); 
         
+        // Lighting Params
+        private static readonly int _MainLightPositionParams = Shader.PropertyToID("_MainLightPosition");
+        private static readonly int _MainLightColorParams = Shader.PropertyToID("_MainLightColor");
+        private static readonly int _MainLightShadowmapTextureParams = Shader.PropertyToID("_MainLightShadowmapTexture");
+        
+        private static readonly int _AdditionalLightsParams = Shader.PropertyToID("_AdditionalLights");
+        private static readonly int _AdditionalLightCountParams = Shader.PropertyToID("_AdditionalLightCount");
+
         private static readonly int _NodeBufferParams = Shader.PropertyToID("_NodeBuffer");
         private static readonly int _PayloadBufferParams = Shader.PropertyToID("_PayloadBuffer");
         private static readonly int _BrickBufferParams = Shader.PropertyToID("_BrickBuffer"); 
@@ -82,6 +91,11 @@ public class VoxelRaytracerFeature : ScriptableRendererFeature
             _settings = settings;
             _shader = settings.raytraceShader;
             renderPassEvent = settings.injectionPoint;
+        }
+        
+        public void Dispose()
+        {
+            _lightBuffer?.Dispose();
         }
 
         public void Setup(Material mat)
@@ -102,8 +116,26 @@ public class VoxelRaytracerFeature : ScriptableRendererFeature
             public Matrix4x4 cameraInverseProjection;
             public int width;
             public int height;
-            public float gridSize; // New
+            public float gridSize;
+            
+            // Lighting Data
+            public Vector4 mainLightPosition;
+            public Vector4 mainLightColor;
+            public TextureHandle shadowMap;
+            
+            public GraphicsBuffer additionalLightsBuffer;
+            public int additionalLightsCount;
         }
+        
+        struct VoxelLight
+        {
+            public Vector4 position; // xyz, w=1 for point/spot
+            public Vector4 color;    // rgb, a=intensity
+            public Vector4 attenuation; // x=range, y=1/range^2
+        }
+        
+        private GraphicsBuffer _lightBuffer;
+        private VoxelLight[] _lightDataArray = new VoxelLight[64]; // Max 64 lights
 
         private class BlitPassData
         {
@@ -115,6 +147,9 @@ public class VoxelRaytracerFeature : ScriptableRendererFeature
         {
             var resourceData = frameData.Get<UniversalResourceData>();
             var cameraData = frameData.Get<UniversalCameraData>();
+            var lightData = frameData.Get<UniversalLightData>();
+            var renderingData = frameData.Get<UniversalRenderingData>();
+            
             var cameraDesc = cameraData.cameraTargetDescriptor;
 
             // 1. Setup Compute Target (Temporary RGBA)
@@ -140,10 +175,83 @@ public class VoxelRaytracerFeature : ScriptableRendererFeature
                 data.cameraInverseProjection = cameraData.camera.projectionMatrix.inverse;
                 data.sourceDepth = resourceData.cameraDepthTexture;
                 data.targetColor = tempResult;
-                data.gridSize = (float)SVOManager.Instance.resolution; // New
+                data.gridSize = (float)SVOManager.Instance.resolution;
+
+                // --- Main Light Setup ---
+                int mainLightIndex = lightData.mainLightIndex;
+                if (mainLightIndex != -1 && mainLightIndex < renderingData.cullResults.visibleLights.Length)
+                {
+                    var visibleLight = renderingData.cullResults.visibleLights[mainLightIndex];
+                    if (visibleLight.lightType == LightType.Directional)
+                    {
+                        Vector4 dir = -visibleLight.localToWorldMatrix.GetColumn(2);
+                        dir.w = 0; // Directional
+                        data.mainLightPosition = dir;
+                    }
+                    else
+                    {
+                        Vector4 pos = visibleLight.localToWorldMatrix.GetColumn(3);
+                        pos.w = 1; // Point/Spot
+                        data.mainLightPosition = pos;
+                    }
+                    data.mainLightColor = visibleLight.finalColor;
+                }
+                else
+                {
+                    data.mainLightPosition = new Vector4(0, 1, 0, 0); 
+                    data.mainLightColor = Color.black;
+                }
+                data.shadowMap = resourceData.mainShadowsTexture;
+
+                // --- Additional Lights Setup ---
+                var visibleLights = renderingData.cullResults.visibleLights;
+                int count = 0;
+                for (int i = 0; i < visibleLights.Length; i++)
+                {
+                    if (i == mainLightIndex) continue;
+                    if (count >= _lightDataArray.Length) break;
+                    
+                    var vl = visibleLights[i];
+                    VoxelLight l = new VoxelLight();
+                    
+                    // Position/Dir
+                     if (vl.lightType == LightType.Directional)
+                    {
+                        Vector4 dir = -vl.localToWorldMatrix.GetColumn(2);
+                        dir.w = 0;
+                        l.position = dir;
+                        l.attenuation = new Vector4(1000, 0, 0, 0); // Infinite range
+                    }
+                    else
+                    {
+                        Vector4 pos = vl.localToWorldMatrix.GetColumn(3);
+                        pos.w = 1;
+                        l.position = pos;
+                        // Attenuation (Simplified range)
+                        float range = vl.range;
+                        l.attenuation = new Vector4(range, 1.0f / (range * range + 0.001f), 0, 0);
+                    }
+                    l.color = vl.finalColor;
+                    
+                    _lightDataArray[count] = l;
+                    count++;
+                }
+
+                if (_lightBuffer == null || _lightBuffer.count < _lightDataArray.Length)
+                {
+                    _lightBuffer?.Dispose();
+                    _lightBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, _lightDataArray.Length, System.Runtime.InteropServices.Marshal.SizeOf<VoxelLight>());
+                }
+                _lightBuffer.SetData(_lightDataArray, 0, 0, count > 0 ? count : 1); // Set at least 1 to avoid errors? Or just count.
+                
+                data.additionalLightsBuffer = _lightBuffer;
+                data.additionalLightsCount = count;
 
                 builder.UseTexture(data.sourceDepth, AccessFlags.Read);
                 builder.UseTexture(data.targetColor, AccessFlags.Write);
+                
+                if (data.shadowMap.IsValid())
+                    builder.UseTexture(data.shadowMap, AccessFlags.Read);
 
                 builder.SetRenderFunc((PassData passData, ComputeGraphContext ctx) =>
                 {
@@ -157,10 +265,21 @@ public class VoxelRaytracerFeature : ScriptableRendererFeature
 
                     cmd.SetComputeMatrixParam(cs, _CameraToWorldParams, passData.cameraToWorld);
                     cmd.SetComputeMatrixParam(cs, _CameraInverseProjectionParams, passData.cameraInverseProjection);
-                    cmd.SetComputeFloatParam(cs, _GridSizeParams, passData.gridSize); // New
+                    cmd.SetComputeFloatParam(cs, _GridSizeParams, passData.gridSize);
 
                     cmd.SetComputeTextureParam(cs, kernel, _CameraDepthTextureParams, passData.sourceDepth);
                     cmd.SetComputeTextureParam(cs, kernel, _ResultParams, passData.targetColor);
+                    
+                    // Main Light
+                    cmd.SetComputeVectorParam(cs, _MainLightPositionParams, passData.mainLightPosition);
+                    cmd.SetComputeVectorParam(cs, _MainLightColorParams, passData.mainLightColor);
+                    
+                    if (passData.shadowMap.IsValid())
+                        cmd.SetComputeTextureParam(cs, kernel, _MainLightShadowmapTextureParams, passData.shadowMap);
+                        
+                    // Additional Lights
+                    cmd.SetComputeBufferParam(cs, kernel, _AdditionalLightsParams, passData.additionalLightsBuffer);
+                    cmd.SetComputeIntParam(cs, _AdditionalLightCountParams, passData.additionalLightsCount);
 
                     int groupsX = Mathf.CeilToInt(passData.width / 8.0f);
                     int groupsY = Mathf.CeilToInt(passData.height / 8.0f);
