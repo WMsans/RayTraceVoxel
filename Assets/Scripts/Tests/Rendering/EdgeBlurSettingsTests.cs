edafbd9899393336fc404f5c2bcc2c1ca3f07623
Jeremy <ggajy123@gmail.com>
2026-02-19 14:04:14 -0800

    test: add edge blur settings defaults

---
+++ Assets/Scripts/Tests/Rendering/EdgeBlurSettingsTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+using VoxelEngine.Core.Rendering;
+
+public class EdgeBlurSettingsTests
+{
+    [Test]
+    public void Defaults_AreWithinExpectedRanges()
+    {
+        var settings = new VoxelRaytracerSettings();
+        Assert.That(settings.edgeWidthPercent, Is.InRange(0.01f, 0.5f));
+        Assert.That(settings.edgeRenderScale, Is.InRange(0.1f, 1.0f));
+        Assert.That(settings.enableEdgeBlur, Is.True);
+    }
+}
