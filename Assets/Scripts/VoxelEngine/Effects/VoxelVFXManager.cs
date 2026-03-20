52d24fadf106fe054dd90171dacff0d5e2e7a0c5
Jeremy <ggajy123@gmail.com>
2026-03-20 13:30:48 -0700

    feat: add VoxelVFXManager for debris particle spawning

---
+++ Assets/Scripts/VoxelEngine/Effects/VoxelVFXManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace VoxelEngine.Core.Effects
+{
+    public class VoxelVFXManager : MonoBehaviour
+    {
+        public static VoxelVFXManager Instance { get; private set; }
+
+        [Header("VFX Configuration")]
+        [SerializeField] private VisualEffect debrisVFXPrefab;
+        [SerializeField] private int poolSize = 3;
+
+        private VisualEffect[] _vfxPool;
+        private int _poolIndex;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            Instance = this;
+            
+            InitializePool();
+        }
+
+        private void InitializePool()
+        {
+            _vfxPool = new VisualEffect[poolSize];
+            for (int i = 0; i < poolSize; i++)
+            {
+                _vfxPool[i] = Instantiate(debrisVFXPrefab, transform);
+                _vfxPool[i].Stop();
+            }
+        }
+
+        public void SpawnDebris(Vector3 position, float radius, int materialId)
+        {
+            VisualEffect vfx = _vfxPool[_poolIndex];
+            _poolIndex = (_poolIndex + 1) % poolSize;
+
+            vfx.transform.position = position;
+            vfx.SetVector3("Position", position);
+            vfx.SetFloat("Radius", radius);
+            vfx.SetInt("MaterialID", materialId);
+            
+            vfx.Reinit();
+            vfx.Play();
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+        }
+    }
+}
